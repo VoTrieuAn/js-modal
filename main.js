27ee6c8ada121b0ae46094766c45fd83dd72eb09
VoTrieuAn <voan2068@gmail.com>
2025-09-04 20:27:47 +0700

    3. Xử lý nhiều modal cùng hoạt động

--- main.js
+++ main.js
@@ -1,25 +1,43 @@
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
-const modal = $("#modal");
+let currentModal = null;
 
-$("#open-modal").onclick = function () {
-  modal.classList.add("show");
-};
+$$("[data-modal]").forEach((btn) => {
+  btn.onclick = function () {
+    // Do tên attribute là data-modal nên có thể dùng dataset để lấy
+    const modal = $(this.dataset.modal);
+    if (modal) {
+      modal.classList.add("show");
+      currentModal = modal;
+    } else {
+      console.error(`${this.dataset.modal} dose not exist`);
+    }
+  };
+});
 
-$("#modal-close").onclick = function () {
-  modal.classList.remove("show");
-};
+$$(".modal-close").forEach((btn) => {
+  btn.onclick = function () {
+    const modal = this.closest(".modal-backdrop");
+    if (modal) {
+      modal.classList.remove("show");
+      currentModal = null;
+    }
+  };
+});
 
-modal.onclick = function (e) {
-  // Kiểm tra để có thể xuất hiện sự kiện bubbling
-  if (e.target === modal) {
-    modal.classList.remove("show");
-  }
-};
+$$(".modal-backdrop").forEach((modal) => {
+  modal.onclick = function (e) {
+    if (e.target === this) {
+      this.classList.remove("show");
+      currentModal = null;
+    }
+  };
+});
 
 document.addEventListener("keydown", function (e) {
   if (e.key === "Escape") {
-    modal.classList.remove("show");
+    currentModal.classList.remove("show");
+    currentModal = null;
   }
 });

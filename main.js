ed950b60872e079a2a402f4ad06c58adc3b45311
VoTrieuAn <voan2068@gmail.com>
2025-09-05 12:12:55 +0700

    8. Tối ưu hàm getScrollbarWidth

--- main.js
+++ main.js
@@ -2,6 +2,30 @@ const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
 function Modal() {
+  // Caching value
+  function getScrollbarWidth() {
+    if (getScrollbarWidth.value) {
+      return getScrollbarWidth.value;
+    }
+    const div = document.createElement("div");
+
+    Object.assign(div.style, {
+      overflow: "scroll",
+      position: "absolute",
+      top: "-9999px",
+    });
+
+    document.body.appendChild(div);
+
+    const scrollbarWidth = div.offsetWidth - div.clientWidth;
+
+    document.body.removeChild(div);
+
+    getScrollbarWidth.value = scrollbarWidth;
+
+    return scrollbarWidth;
+  }
+
   this.openModal = (options = {}) => {
     const { templateId, allowBackdropClose = true } = options;
     const template = $(`#${templateId}`);
@@ -104,21 +128,3 @@ $("#open-modal-2").onclick = () => {
     };
   }
 };
-
-function getScrollbarWidth() {
-  const div = document.createElement("div");
-
-  Object.assign(div.style, {
-    overflow: "scroll",
-    position: "absolute",
-    top: "-9999px",
-  });
-
-  document.body.appendChild(div);
-
-  const scrollbarWidth = div.offsetWidth - div.clientWidth;
-
-  document.body.removeChild(div);
-
-  return scrollbarWidth;
-}

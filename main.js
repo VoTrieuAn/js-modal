658523adacf8cda29ac82133f74a18421b442778
VoTrieuAn <voan2068@gmail.com>
2025-09-05 14:05:34 +0700

    9. Sửa logic Modal đáp ứng options mới

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
-function Modal() {
+function Modal(options = {}) {
+  const { templateId, closeMethods = ["button", "overlay", "escape"] } =
+    options;
+  const template = $(`#${templateId}`);
+
+  if (!template) {
+    console.error(`#${templateId} dose not exist`);
+    return;
+  }
+
+  // Khi đặt tên dấu gạch dưới trước phương thức, thì sẽ ngầm hiểu là không dùng nó ở đối tượng new [Object]()._allowBackdropClose
+  this._allowButtonClose = closeMethods.includes("button");
+  this._allowBackdropClose = closeMethods.includes("overlay");
+  this._allowEscapeClose = closeMethods.includes("escape");
   // Caching value
   function getScrollbarWidth() {
     if (getScrollbarWidth.value) {
@@ -26,15 +39,7 @@ function Modal() {
     return scrollbarWidth;
   }
 
-  this.openModal = (options = {}) => {
-    const { templateId, allowBackdropClose = true } = options;
-    const template = $(`#${templateId}`);
-
-    if (!template) {
-      console.error(`#${templateId} dose not exist`);
-      return;
-    }
-
+  this.open = () => {
     /**
      * cloneNode(boolean)
      * Dùng để nhân bản Node
@@ -48,48 +53,57 @@ function Modal() {
     backdrop.className = "modal-backdrop";
     const container = document.createElement("div");
     container.className = "modal-container";
-    const closeBtn = document.createElement("div");
-    closeBtn.className = "modal-close";
-    closeBtn.innerHTML = "&times;";
+
+    if (this._allowButtonClose) {
+      const closeBtn = document.createElement("div");
+      closeBtn.className = "modal-close";
+      closeBtn.innerHTML = "&times;";
+
+      container.append(closeBtn);
+
+      closeBtn.onclick = () => {
+        this.close(backdrop);
+      };
+    }
+
     const modalContent = document.createElement("div");
     modalContent.className = "modal-content";
 
     // Append content and elements
     modalContent.append(content);
-    container.append(closeBtn, modalContent);
+    container.append(modalContent);
     backdrop.append(container);
     document.body.append(backdrop);
+
+    // Disable scrolling
+    document.body.classList.add("no-scroll");
+    document.body.style.paddingRight = getScrollbarWidth() + "px";
 
     setTimeout(() => {
       backdrop.classList.add("show");
     }, 0);
 
     // Attach event listeners
-    closeBtn.onclick = () => {
-      this.closeModal(backdrop);
-    };
-
-    if (allowBackdropClose) {
+    if (this._allowBackdropClose) {
       backdrop.onclick = (e) => {
         if (e.target === backdrop) {
-          this.closeModal(backdrop);
+          this.close(backdrop);
         }
       };
     }
 
-    document.addEventListener("keydown", (e) => {
-      if (e.key === "Escape") {
-        this.closeModal(backdrop);
-      }
-    });
+    if (this._allowEscapeClose) {
+      document.addEventListener("keydown", (e) => {
+        if (e.key === "Escape") {
+          this.close(backdrop);
+        }
+      });
+    }
 
-    // Disable scrolling
-    document.body.classList.add("no-scroll");
-    document.body.style.paddingRight = getScrollbarWidth() + "px";
     return backdrop;
   };
 
-  this.closeModal = (modalElement) => {
+  this.close = (modalElement) => {
     modalElement.classList.remove("show");
     modalElement.ontransitionend = function () {
       modalElement.remove();
@@ -101,19 +115,16 @@ function Modal() {
   };
 }
 
-const modal = new Modal();
-// modal.openModal("<h1>Hello An Vo </h1>");
+const modal = new Modal({
+  templateId: "modal-1",
+});
+// modal.open("<h1>Hello An Vo </h1>");
 $("#open-modal-1").onclick = () => {
-  modal.openModal({
-    templateId: "modal-1",
-  });
+  modal.open();
 };
 
 $("#open-modal-2").onclick = () => {
-  const modalElement = modal.openModal({
-    templateId: "modal-2",
-    allowBackdropClose: false,
-  });
+  const modalElement = modal.open();
 
   const form = modalElement.querySelector("#login-form");
   if (form) {
@@ -128,3 +139,15 @@ $("#open-modal-2").onclick = () => {
     };
   }
 };
+
+// Yêu cầu
+// 0. Tạo ra từng đối tượng modal để dễ quản lý
+// 1. modal.open()
+// 2. modal.close() ==> Chỉ gỡ class show chứ chưa gỡ khỏi DOM
+// 3. modal.setFooterContent("Html string");
+// 4. modal.addFooterButton("Cancel", "class-1 class-2", (e) => {})
+// 5. modal.addFooterButton("Agree", "class-3 class-4", (e) => {})
+// 6. modal.destroy() ==> Gỡ khởi DOM luôn
+// Chia yêu cầu làm 2 loại
+// 1. Yêu cầu độc lập không phụ thuộc
+// 2. Yêu cầu phụ thuộc
